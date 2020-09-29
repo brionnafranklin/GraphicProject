b7c16fd418ac1445331eb9393c661302b9004027
Bri <brionnafranklin@yahoo.com>
2020-09-29 14:28:42 -0500

    set up main using tutorial

--- GraphicsScene/main.cpp
+++ GraphicsScene/main.cpp
@@ -1,6 +1,13 @@
 #include <gl_core_4_4.h>
 #include <GLFW/glfw3.h>
 #include <cstdio>
+#include <Gizmos.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
 
 int main() {
 	if (!glfwInit())
@@ -28,16 +35,45 @@ int main() {
 	int minor = ogl_GetMinorVersion();
 	printf("GL: %i.%i\n", major, minor);
 
+	aie::Gizmos::create(10000, 10000, 10000, 10000);
+
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+
+	glClearColor(0.2f, 0.05f, 0.2f, 1.0f);
+	glEnable(GL_DEPTH_TEST); // enables the depth buffer
+
 	//loop goes here
 	while (!glfwWindowShouldClose(window) &&
 		!glfwGetKey(window, GLFW_KEY_ESCAPE)) 
 	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		aie::Gizmos::clear();
+
+		aie::Gizmos::addTransform(mat4(1));
+
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+
+		for (int i = 0; i < 21; ++i) 
+		{
+			aie::Gizmos::addLine(vec3(-10 + i, 0, 10),
+				vec3(-10 + i, 0, -10),
+				i == 10 ? white : black);
+
+			aie::Gizmos::addLine(vec3(10, 0, -10 + i),
+				vec3(-10, 0, -10 + i),
+				i == 10 ? white : black);
+		}
+
+		aie::Gizmos::draw(projection * view);
 		// our game logic and update code goes here!
 		// so does our render code!
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	aie::Gizmos::destroy();
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
