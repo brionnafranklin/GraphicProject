f9f5700daf8e2f6750d657085f77db70c13335b3
Bri <brionnafranklin@yahoo.com>
2020-09-29 11:58:44 -0500

    render and open gl window

--- GraphicsScene/main.cpp
+++ GraphicsScene/main.cpp
@@ -1,6 +1,44 @@
 #include <gl_core_4_4.h>
 #include <GLFW/glfw3.h>
+#include <cstdio>
 
 int main() {
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Computer Graphics", nullptr, nullptr);
+
+	if (window == nullptr) 
+	{
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+
+	int major = ogl_GetMajorVersion();
+	int minor = ogl_GetMinorVersion();
+	printf("GL: %i.%i\n", major, minor);
+
+	//loop goes here
+	while (!glfwWindowShouldClose(window) &&
+		!glfwGetKey(window, GLFW_KEY_ESCAPE)) 
+	{
+		// our game logic and update code goes here!
+		// so does our render code!
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
